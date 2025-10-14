7fb3fca7c26e69aad7e4a08cf789d99ff54430c7
Fionn Reilly <20101977@mail.wit.ie>
2025-10-14 21:45:17 +0100

    added event handling basics for add button

--- app/src/main/java/ie/setu/mobileappdevelopmentca1/MainActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevelopmentca1/MainActivity.kt
@@ -2,15 +2,30 @@ package ie.setu.mobileappdevelopmentca1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import ie.setu.mobileappdevelopmentca1.databinding.ActivityMainBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
         i("Main Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            val eventTitle = binding.eventTitle.text.toString()
+            if (eventTitle.isNotEmpty()) {
+                i("add Button Pressed: $eventTitle")
+            }
+            else {
+                Snackbar.make(it,"Please nter a title", Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
